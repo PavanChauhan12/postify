18d8bfe9e687feb578942a662c3ba4ad030e90e3
PavanChauhan12 <pavanchauhan1204@gmail.com>
2025-07-03 17:56:04 +0530

    backend initialization

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require('dotenv');
+const authRoute = require("./routes/auth_router");
+dotenv.config();
+
+const app = express();
+
+mongoose.connect(process.env.DB_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/auth', authRoute);
+
+app.listen(3000, () => {
+  console.log("Server started at port: 3000");
+});
