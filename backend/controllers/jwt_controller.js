6ad9b473bfb41977093672d3f1aecda9701a01fc
PavanChauhan12 <pavanchauhan1204@gmail.com>
2025-07-04 13:58:51 +0530

    login and blog routes updated

--- backend/controllers/jwt_controller.js
+++ backend/controllers/jwt_controller.js
@@ -3,8 +3,10 @@ require("dotenv").config();
 const accessTokenSecret = process.env.ACCESS_KEY_SECRET;
 
 const jwtController = {
-    signAccessToken: (email) => {
-        const payload = { email: email };
+    signAccessToken: (user) => {
+        const payload = { 
+            id: user._id,
+            email: user.email };
         return jwt.sign(payload, accessTokenSecret, { expiresIn: '15d' });
     }
 };
